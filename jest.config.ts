cbf23c9c9b36e80babaa2274723267a856fd50a7
jukerah <juka.mebaj@gmail.com>
2023-12-12 00:09:46 -0300

    chore: add tsconfig.json and jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest'
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
+
+export default config
