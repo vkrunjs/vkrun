f775cc38bbc83e18b29753e869bb24f796c171fb
jukerah <juka.mebaj@gmail.com>
2023-12-12 01:13:59 -0300

    test: should be able to validate the required method and return error if the value is not provided

--- src/__tests__/validator.test.ts
+++ src/__tests__/validator.test.ts
@@ -2,14 +2,21 @@ import { validator } from '../index'
 
 describe('Validator', () => {
   it('Should be able to validate the required method and return true if the value is provided', () => {
-    const example = false
-    const sut = validator.required(example)
+    const value = false
+    const sut = validator.required(value)
     expect(sut).toBeTruthy()
   })
 
   it('Should be able to validate the required method and return false if the value is not provided', () => {
-    const example = undefined
-    const sut = validator.required(example)
+    const value = undefined
+    const sut = validator.required(value)
     expect(sut).toBeFalsy()
+  })
+
+  it('Should be able to validate the required method and return error if the value is not provided', () => {
+    const hasNoValue = undefined
+    const error = new Error('error message')
+    const sut = validator.required(hasNoValue, error)
+    expect(sut).toEqual(error)
   })
 })
