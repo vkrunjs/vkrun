69a9c92bb7210145c3597699dce10fb7471fd6e9
jukerah <juka.mebaj@gmail.com>
2023-12-12 21:09:17 -0300

    refactor: change message error

--- src/__tests__/validator.test.ts
+++ src/__tests__/validator.test.ts
@@ -21,7 +21,7 @@ describe('Validator', () => {
 
   it('Should be able to validate the required method and return error if the value is not provided', () => {
     const hasNoValue = undefined
-    const error = new Error('error message')
+    const error = new Error('any_error')
     const sut = validator.required(hasNoValue, error)
     expect(sut).toEqual(error)
   })
