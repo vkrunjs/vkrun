fc8c1894574dea9e0dabf45a259c536f31c41ccc
jukerah <juka.mebaj@gmail.com>
2023-12-25 02:38:12 -0300

    test: add new values for translation

--- src/lib/errors/__tests__/set-translation-message.test.ts
+++ src/lib/errors/__tests__/set-translation-message.test.ts
@@ -17,7 +17,64 @@ describe('Set translation message', () => {
           },
           minWord: {
             noMinimumWords: '[valueName] deve ter pelo menos [minWord] palavras!'
+          },
+          uuid: {
+            strict: '[valueName] deve ser do tipo uuid!'
+          },
+          email: {
+            strict: 'email [value] é inválido!'
+          },
+          maxLength: {
+            strict: '[valueName] deve ter no máximo [maxLength] caracteres!'
+          },
+          minLength: {
+            strict: '[valueName] deve ter no mínimo [minLength] caracteres!'
+          },
+          number: {
+            strict: '[valueName] deve ser um tipo number!'
+          },
+          float: {
+            strict: '[valueName] deve ser um number e float!'
+          },
+          integer: {
+            strict: '[valueName] deve ser um number e integer!'
+          },
+          boolean: {
+            strict: '[valueName] deve ser do tipo boolean!'
+          },
+          required: {
+            strict: '[valueName] é obrigatório!'
+          },
+          date: {
+            invalidFormat: 'a data [valueName] não está no formato [type]!',
+            invalidParameter: 'método date recebeu parâmetro inválido: type é obrigatório!'
+          },
+          dateGreaterThan: {
+            invalidDate: 'a data fornecida é inválida!',
+            limitExceeded: 'a data [valueName] deve ser maior que a data de referência!'
+          },
+          dateLessThan: {
+            invalidDate: 'a data fornecida é inválida!',
+            limitExceeded: 'a data [valueName] deve ser menor que a data de referência!'
+          },
+          time: {
+            invalidParameter: 'o método time recebeu parâmetro inválido: o type é obrigatório!',
+            invalidFormat: 'a hora [value] não está no formato [type]!'
           }
+        }
+      },
+      schema: {
+        validateProperty: {
+          itemArray: {
+            valueName: 'todos os valores em [keyName]'
+          }
+        },
+        validateSchema: {
+          keyNotDeclaredInTheSchema: 'a chave [keyName] não foi declarada no schema'
+        },
+        validateObject: {
+          schemaKeyAbsent: 'a chave [keyName] é obrigatória!',
+          notIsArray: 'o valor [keyName] deve ser um array!'
         }
       }
     }
@@ -25,9 +82,6 @@ describe('Set translation message', () => {
     const sut = setTranslationMessage(newMessages)
 
     expect(sut).toBeTruthy()
-    expect(errorMessage.validex.constructorParams.valueName.invalidClassParam).toEqual(newMessages.validex.constructorParams.valueName.invalidClassParam)
-    expect(errorMessage.validex.constructorParams.valueName.missingClassParam).toEqual(newMessages.validex.constructorParams.valueName.missingClassParam)
-    expect(errorMessage.validex.method.string.strict).toEqual(newMessages.validex.method.string.strict)
-    expect(errorMessage.validex.method.minWord.noMinimumWords).toEqual(newMessages.validex.method.minWord.noMinimumWords)
+    expect(errorMessage).toEqual(newMessages)
   })
 })
