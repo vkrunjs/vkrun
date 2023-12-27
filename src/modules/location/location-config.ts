ca5858b904f715983a443a557e4e7b978c79c462
jukerah <juka.mebaj@gmail.com>
2023-12-26 22:21:31 -0300

    refactor: translation modules

---
+++ src/modules/location/location-config.ts
@@ -0,0 +1,30 @@
+export const locationConfig: Array<{ keys: string[], reservedKeys?: string[] }> = [
+  // Validator
+  { keys: ['validator', 'constructorParams', 'valueName', 'invalidClassParam'] },
+  { keys: ['validator', 'constructorParams', 'valueName', 'missingClassParam'] },
+  { keys: ['validator', 'method', 'string', 'strict'], reservedKeys: ['[valueName]'] },
+  { keys: ['validator', 'method', 'minWord', 'noMinimumWords'], reservedKeys: ['[valueName]', '[minWord]'] },
+  { keys: ['validator', 'method', 'uuid', 'strict'], reservedKeys: ['[valueName]'] },
+  { keys: ['validator', 'method', 'email', 'strict'], reservedKeys: ['[value]'] },
+  { keys: ['validator', 'method', 'maxLength', 'strict'], reservedKeys: ['[valueName]', '[maxLength]'] },
+  { keys: ['validator', 'method', 'minLength', 'strict'], reservedKeys: ['[valueName]', '[minLength]'] },
+  { keys: ['validator', 'method', 'number', 'strict'], reservedKeys: ['[valueName]'] },
+  { keys: ['validator', 'method', 'float', 'strict'], reservedKeys: ['[valueName]'] },
+  { keys: ['validator', 'method', 'integer', 'strict'], reservedKeys: ['[valueName]'] },
+  { keys: ['validator', 'method', 'boolean', 'strict'], reservedKeys: ['[valueName]'] },
+  { keys: ['validator', 'method', 'required', 'strict'], reservedKeys: ['[valueName]'] },
+  { keys: ['validator', 'method', 'date', 'invalidFormat'], reservedKeys: ['[valueName]', '[type]'] },
+  { keys: ['validator', 'method', 'date', 'invalidParameter'] },
+  { keys: ['validator', 'method', 'dateGreaterThan', 'invalidDate'] },
+  { keys: ['validator', 'method', 'dateGreaterThan', 'limitExceeded'], reservedKeys: ['[valueName]'] },
+  { keys: ['validator', 'method', 'dateLessThan', 'invalidDate'] },
+  { keys: ['validator', 'method', 'dateLessThan', 'limitExceeded'], reservedKeys: ['[valueName]'] },
+  { keys: ['validator', 'method', 'time', 'invalidParameter'] },
+  { keys: ['validator', 'method', 'time', 'invalidFormat'], reservedKeys: ['[value]', '[type]'] },
+
+  // Schema
+  { keys: ['schema', 'validateProperty', 'itemArray', 'valueName'], reservedKeys: ['[keyName]'] },
+  { keys: ['schema', 'validateSchema', 'keyNotDeclaredInTheSchema'], reservedKeys: ['[keyName]'] },
+  { keys: ['schema', 'validateObject', 'schemaKeyAbsent'], reservedKeys: ['[keyName]'] },
+  { keys: ['schema', 'validateObject', 'notIsArray'], reservedKeys: ['[keyName]'] }
+]
--- src/modules/location/set-translation-message.ts
+++ src/modules/location/set-translation-message.ts
@@ -1,4 +1,5 @@
 import { AnyInformativeMessage, SetTranslationMessage } from '../types'
+import { locationConfig } from './location-config'
 import { informativeMessage } from './informative-message'
 
 export const setTranslationMessage = (newMessages: SetTranslationMessage): boolean => {
@@ -12,19 +13,19 @@ export const setTranslationMessage = (newMessages: SetTranslationMessage): boole
   }
 
   const setValue = (keys: string[], value: string | undefined, reservedKeys?: string[]): void => {
-    const keyPath: string = keys.join('.')
+    const keyPath = keys.join('.')
     let currentObject: AnyInformativeMessage = informativeMessage
 
     if (isString(value, keyPath)) {
       for (let i = 0; i < keys.length - 1; i++) {
-        const currentKey: string = keys[i]
+        const currentKey = keys[i]
         currentObject = currentObject[currentKey]
       }
 
-      const lastKey: string = keys[keys.length - 1]
+      const lastKey = keys[keys.length - 1]
 
       if (reservedKeys) {
-        const hasReservedKeys = reservedKeys.every((k: string) => String(value).includes(k))
+        const hasReservedKeys = reservedKeys.every((k) => String(value).includes(k))
         if (!hasReservedKeys) {
           const reservedKeysString = reservedKeys.join(' and ')
           throw new Error(`setTranslationMessage: ${lastKey} must contain the reserved key(s) ${reservedKeysString}!`)
@@ -35,52 +36,15 @@ export const setTranslationMessage = (newMessages: SetTranslationMessage): boole
     }
   }
 
-  const configurations = [
-    // Validator
-    { keys: ['validator', 'constructorParams', 'valueName', 'invalidClassParam'] },
-    { keys: ['validator', 'constructorParams', 'valueName', 'missingClassParam'] },
-    { keys: ['validator', 'method', 'string', 'strict'], reservedKeys: ['[valueName]'] },
-    { keys: ['validator', 'method', 'minWord', 'noMinimumWords'], reservedKeys: ['[valueName]', '[minWord]'] },
-    { keys: ['validator', 'method', 'uuid', 'strict'], reservedKeys: ['[valueName]'] },
-    { keys: ['validator', 'method', 'email', 'strict'], reservedKeys: ['[value]'] },
-    { keys: ['validator', 'method', 'maxLength', 'strict'], reservedKeys: ['[valueName]', '[maxLength]'] },
-    { keys: ['validator', 'method', 'minLength', 'strict'], reservedKeys: ['[valueName]', '[minLength]'] },
-    { keys: ['validator', 'method', 'number', 'strict'], reservedKeys: ['[valueName]'] },
-    { keys: ['validator', 'method', 'float', 'strict'], reservedKeys: ['[valueName]'] },
-    { keys: ['validator', 'method', 'integer', 'strict'], reservedKeys: ['[valueName]'] },
-    { keys: ['validator', 'method', 'boolean', 'strict'], reservedKeys: ['[valueName]'] },
-    { keys: ['validator', 'method', 'required', 'strict'], reservedKeys: ['[valueName]'] },
-    { keys: ['validator', 'method', 'date', 'invalidFormat'], reservedKeys: ['[valueName]', '[type]'] },
-    { keys: ['validator', 'method', 'date', 'invalidParameter'] },
-    { keys: ['validator', 'method', 'dateGreaterThan', 'invalidDate'] },
-    { keys: ['validator', 'method', 'dateGreaterThan', 'limitExceeded'], reservedKeys: ['[valueName]'] },
-    { keys: ['validator', 'method', 'dateLessThan', 'invalidDate'] },
-    { keys: ['validator', 'method', 'dateLessThan', 'limitExceeded'], reservedKeys: ['[valueName]'] },
-    { keys: ['validator', 'method', 'time', 'invalidParameter'] },
-    { keys: ['validator', 'method', 'time', 'invalidFormat'], reservedKeys: ['[value]', '[type]'] },
-
-    // Schema
-    { keys: ['schema', 'validateProperty', 'itemArray', 'valueName'], reservedKeys: ['[keyName]'] },
-    { keys: ['schema', 'validateSchema', 'keyNotDeclaredInTheSchema'], reservedKeys: ['[keyName]'] },
-    { keys: ['schema', 'validateObject', 'schemaKeyAbsent'], reservedKeys: ['[keyName]'] },
-    { keys: ['schema', 'validateObject', 'notIsArray'], reservedKeys: ['[keyName]'] }
-  ]
-
-  configurations.forEach((config: { keys: string[], reservedKeys?: string[] }) => {
+  locationConfig.forEach((config) => {
     const messages: any = newMessages
-    if (config.keys.length === 3) {
-      setValue(
-        config.keys,
-        messages?.[config.keys[0]]?.[config.keys[1]]?.[config.keys[2]],
-        config.reservedKeys
-      )
-    } else if (config.keys.length === 4) {
-      setValue(
-        config.keys,
-        messages?.[config.keys[0]]?.[config.keys[1]]?.[config.keys[2]]?.[config.keys[3]],
-        config.reservedKeys
-      )
+    let value = messages
+
+    for (const key of config.keys) {
+      value = value?.[key]
     }
+
+    setValue(config.keys, value, config.reservedKeys)
   })
 
   return true
