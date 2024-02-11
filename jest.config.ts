b7108d1ab7bba17e68dbddc8f78ee4802557876d
jukerah <juka.mebaj@gmail.com>
2024-02-11 03:07:48 -0300

    refactor: move files

--- jest.config.ts
+++ jest.config.ts
@@ -20,8 +20,11 @@ const config: Config = {
   coveragePathIgnorePatterns: [
     '<rootDir>/src/.*\\.d\\.ts$',
     '<rootDir>/src/index.ts$',
-    '<rootDir>/src/modules/types.ts$',
-    '<rootDir>/src/modules/location/index.ts$'
+    '<rootDir>/src/modules/location/index.ts$',
+    '<rootDir>/src/modules/location/types.ts$',
+    '<rootDir>/src/modules/logger/types.ts$',
+    '<rootDir>/src/modules/utils/types.ts$',
+    '<rootDir>/src/modules/schema/types.ts$',
   ]
 }
 
