85bb9fb845f0c62ba4910b57d9b688f7888feda9
jukerah <juka.mebaj@gmail.com>
2024-02-11 03:16:37 -0300

    feat: date to string types

---
+++ src/modules/utils/types.ts
@@ -0,0 +1,18 @@
+export type StringToDateTypes = 'ISO8601' | 'DD/MM/YYYY' | 'MM/DD/YYYY' | 'DD-MM-YYYY' | 'MM-DD-YYYY' | 'YYYY/MM/DD' | 'YYYY/DD/MM' | 'YYYY-MM-DD' | 'YYYY-DD-MM'
+
+export type DateToStringTypes =
+  'YYYY/MM/DD HH:MM:SS.MS' |
+  'YYYY/MM/DD HH:MM:SS' |
+  'YYYY/MM/DD HH:MM' |
+  'YYYY/MM/DD' |
+  'MM/DD/YYYY HH:MM:SS.MS' |
+  'MM/DD/YYYY HH:MM:SS' |
+  'MM/DD/YYYY HH:MM' |
+  'MM/DD/YYYY' |
+  'DD/MM/YYYY HH:MM:SS.MS' |
+  'DD/MM/YYYY HH:MM:SS' |
+  'DD/MM/YYYY HH:MM' |
+  'DD/MM/YYYY' |
+  'HH:MM:SS.MS' |
+  'HH:MM:SS' |
+  'HH:MM'
