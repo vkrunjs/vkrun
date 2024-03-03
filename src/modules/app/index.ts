8bdf9afc22470a180786557212815b5e1b7cbcfc
jukerah <juka.mebaj@gmail.com>
2024-03-03 20:46:29 -0300

    feat: create app to create a server to use routes and middleware

---
+++ src/modules/app/index.ts
@@ -0,0 +1,27 @@
+import * as http from 'http'
+import { VkrunRouter } from '../router'
+import { customResponse } from '../router/custom-response'
+import * as type from '../types'
+
+export const app = (): type.App => {
+  let router: VkrunRouter
+  const middlewares: any[] = []
+
+  const server = (): type.CreateServer => {
+    return http.createServer((request, _response) => {
+      const response = customResponse(_response)
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      router.handleRequest(request, response, middlewares)
+    })
+  }
+
+  const use = (middleware: Record<string, any>): void => {
+    if (middleware instanceof VkrunRouter) {
+      router = middleware
+    } else {
+      middlewares.push(middleware)
+    }
+  }
+
+  return { server, use }
+}
