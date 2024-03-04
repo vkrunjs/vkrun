54522216be33fd255dfa5c589a2382466c9c8536
jukerah <juka.mebaj@gmail.com>
2024-03-03 22:02:40 -0300

    feat: add new module exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import { schema } from './modules/schema'
-import { setLocation } from './modules/location'
+import { app } from './modules/app'
+import { schema, setLocation } from './modules/schema'
 import { createLogger } from './modules/logger'
+import { Router } from './modules/router'
+import {
+  parseData,
+  cors,
+  rateLimit,
+  Session,
+  validateRouteData
+} from './modules/middleware'
+import { jwt } from './modules/jwt'
+export * from './modules/router/adapters'
+export * from './modules/types'
 
-const vkrun = { schema, setLocation, createLogger }
-export default vkrun
-export { schema, setLocation, createLogger }
+export default app
+export {
+  cors,
+  schema,
+  setLocation,
+  createLogger,
+  Router,
+  parseData,
+  rateLimit,
+  jwt,
+  Session,
+  validateRouteData
+}
