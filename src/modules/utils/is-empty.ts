616e26f6866e62ad8ee605c3a81dba2368deb5bc
jukerah <juka.mebaj@gmail.com>
2024-03-03 23:10:39 -0300

    feat: util is empty validate empty object

--- src/modules/utils/is-empty.ts
+++ src/modules/utils/is-empty.ts
@@ -1,8 +1,9 @@
 import { isArray } from './is-array'
 import { isString } from './is-string'
 
-export const isEmpty = (value: string | any[]): value is '' => {
+export const isEmpty = (value: string | any[] | object): value is '' => {
   if (isString(value) && value === '') return true
   if (isArray(value) && value.length === 0) return true
+  if (typeof value === 'object' && Object.keys(value).length === 0) return true
   return false
 }
