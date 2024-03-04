0684b1ae3bcba0c1c5ed0c285c8cc277f7d0c4be
jukerah <juka.mebaj@gmail.com>
2024-03-04 01:12:08 -0300

    refactor: add utils validation

--- src/modules/utils/is-not-empty.ts
+++ src/modules/utils/is-not-empty.ts
@@ -1,9 +1,10 @@
 import { isArray } from './is-array'
+import { isObject } from './is-object'
 import { isString } from './is-string'
 
-export const isNotEmpty = (value: string | any[]): boolean => {
+export const isNotEmpty = (value: string | any[] | object): boolean => {
   if (isString(value) && value !== '') return true
   if (isArray(value) && value.length > 0) return true
-  if (typeof value === 'object' && Object.keys(value).length > 0) return true
+  if (isObject(value) && Object.keys(value).length > 0) return true
   return false
 }
