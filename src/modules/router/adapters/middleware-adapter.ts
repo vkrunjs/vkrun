c0fa5f2cc7428e0831c1e51c4feb61f8e0d4006a
jukerah <juka.mebaj@gmail.com>
2024-03-03 21:54:19 -0300

    feat: add route adapters

---
+++ src/modules/router/adapters/middleware-adapter.ts
@@ -0,0 +1,7 @@
+import * as type from '../../types'
+
+export const middlewareAdapter = (middleware: type.Middleware): any => {
+  return (request: type.Request, response: type.Response, next: type.NextFunction) => {
+    middleware.handle(request, response, next)
+  }
+}
