1d474f8cedd077187b3e4059fe051dd802a409e3
jukerah <juka.mebaj@gmail.com>
2024-03-03 23:02:47 -0300

    test: add util is boolean

---
+++ src/modules/utils/__tests__/is-boolean.spec.ts
@@ -0,0 +1,20 @@
+import { isBoolean } from '../is-boolean'
+
+describe('isBoolean', () => {
+  it('should return true for boolean values', () => {
+    expect(isBoolean(true)).toBeTruthy()
+    expect(isBoolean(false)).toBeTruthy()
+  })
+
+  it('should return false for non-boolean values', () => {
+    expect(isBoolean(undefined)).toBeFalsy()
+    expect(isBoolean(null)).toBeFalsy()
+    expect(isBoolean(0)).toBeFalsy()
+    expect(isBoolean(1)).toBeFalsy()
+    expect(isBoolean('true')).toBeFalsy()
+    expect(isBoolean('false')).toBeFalsy()
+    expect(isBoolean([])).toBeFalsy()
+    expect(isBoolean({})).toBeFalsy()
+    expect(isBoolean(() => {})).toBeFalsy()
+  })
+})
