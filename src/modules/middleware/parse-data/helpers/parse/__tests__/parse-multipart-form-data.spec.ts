b54d92cd9dea7a15bf321b31f6d8923b1d31ec30
jukerah <juka.mebaj@gmail.com>
2024-03-03 21:40:52 -0300

    test: add parse multipart form data

---
+++ src/modules/middleware/parse-data/helpers/parse/__tests__/parse-multipart-form-data.spec.ts
@@ -0,0 +1,76 @@
+import { parseMultipartFormData } from '../parse-multipart-form-data'
+
+describe('Parse Data - Parse Multipart Form Data', () => {
+  it('Should parse body and return an object with the data if the body is not empty', () => {
+    const request: any = {
+      headers: {
+        'content-type': 'multipart/form-data; boundary=--------------------------069109777880514251234306'
+      },
+      body: '----------------------------069109777880514251234306\r\n' +
+      'Content-Disposition: form-data; name="string"\r\n' +
+      '\r\n' +
+      'any@mail.com\r\n' +
+      '----------------------------069109777880514251234306\r\n' +
+      'Content-Disposition: form-data; name="integer"\r\n' +
+      '\r\n' +
+      '123\r\n' +
+      '----------------------------069109777880514251234306\r\n' +
+      'Content-Disposition: form-data; name="float"\r\n' +
+      '\r\n' +
+      '1.56\r\n' +
+      '----------------------------069109777880514251234306\r\n' +
+      'Content-Disposition: form-data; name="boolean"\r\n' +
+      '\r\n' +
+      'true\r\n' +
+      '----------------------------069109777880514251234306\r\n' +
+      'Content-Disposition: form-data; name="date"\r\n' +
+      '\r\n' +
+      '2000-02-03T02:00:00.000Z\r\n' +
+      '----------------------------069109777880514251234306\r\n' +
+      'Content-Disposition: form-data; name="file"; filename="filename.txt"\r\n' +
+      'Content-Type: text/plain\r\n' +
+      '\r\n' +
+      '----------------------------069109777880514251234306--'
+    }
+
+    const sut = parseMultipartFormData(request, false)
+
+    expect(sut).toEqual({
+      string: 'any@mail.com',
+      integer: 123,
+      float: 1.56,
+      boolean: true,
+      date: new Date('2000-02-03T02:00:00.000Z')
+    })
+  })
+
+  it('Should parse body and return an object with the data if the body is not empty with escape SQL', () => {
+    const request: any = {
+      headers: {
+        'content-type': 'multipart/form-data; boundary=--------------------------041369582845125703494629'
+      },
+      body: '----------------------------041369582845125703494629\r\n' +
+      'Content-Disposition: form-data; name="sql"\r\n' +
+      '\r\n' +
+      'SELECT * FROM USER;\r\n' +
+      '----------------------------041369582845125703494629--'
+    }
+
+    const sut = parseMultipartFormData(request, true)
+
+    expect(sut).toEqual({
+      sql: "'SELECT * FROM USER;'"
+    })
+  })
+
+  it('Should parse body and return an object empty if the body is empty', () => {
+    const request: any = {
+      headers: {},
+      body: ''
+    }
+
+    const sut = parseMultipartFormData(request, false)
+
+    expect(sut).toEqual({})
+  })
+})
