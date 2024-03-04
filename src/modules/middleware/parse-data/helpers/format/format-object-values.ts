028282080bda75d89ceeae103703dd047f71676b
jukerah <juka.mebaj@gmail.com>
2024-03-03 21:36:19 -0300

    feat: add format object values

---
+++ src/modules/middleware/parse-data/helpers/format/format-object-values.ts
@@ -0,0 +1,14 @@
+import { formatValue } from './format-value'
+
+export const formatObjectValues = (obj: Record<string, any>, escapeSQL: boolean): Record<string, string | number | boolean | Date> => {
+  const formattedObj: Record<string, any> = {}
+
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const value = obj[key]
+      formattedObj[key] = formatValue(value, escapeSQL)
+    }
+  }
+
+  return formattedObj
+}
