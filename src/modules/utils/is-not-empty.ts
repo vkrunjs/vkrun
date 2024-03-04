ab9b0f81685a0918e2cb95d912349491e3c389c0
jukerah <juka.mebaj@gmail.com>
2024-03-04 01:10:07 -0300

    feat: validate if object is not empty

--- src/modules/utils/is-not-empty.ts
+++ src/modules/utils/is-not-empty.ts
@@ -4,5 +4,6 @@ import { isString } from './is-string'
 export const isNotEmpty = (value: string | any[]): boolean => {
   if (isString(value) && value !== '') return true
   if (isArray(value) && value.length > 0) return true
+  if (typeof value === 'object' && Object.keys(value).length > 0) return true
   return false
 }
