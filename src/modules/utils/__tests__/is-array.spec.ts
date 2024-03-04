b9e258f5271b51d2c7df56342a12a19be703b002
jukerah <juka.mebaj@gmail.com>
2024-03-03 23:00:05 -0300

    test: add util  is array

---
+++ src/modules/utils/__tests__/is-array.spec.ts
@@ -0,0 +1,21 @@
+import { isArray } from '../is-array'
+
+describe('isArray', () => {
+  it('should return true for an array', () => {
+    expect(isArray([])).toBeTruthy()
+    expect(isArray([1, 2, 3])).toBeTruthy()
+    expect(isArray(['a', 'b', 'c'])).toBeTruthy()
+  })
+
+  it('should return false for non-array values', () => {
+    expect(isArray(undefined)).toBeFalsy()
+    expect(isArray(null)).toBeFalsy()
+    expect(isArray(42)).toBeFalsy()
+    expect(isArray('array')).toBeFalsy()
+    expect(isArray({})).toBeFalsy()
+    expect(isArray({ length: 0 })).toBeFalsy()
+    expect(isArray(new Set())).toBeFalsy()
+    expect(isArray(new Map())).toBeFalsy()
+    expect(isArray(() => {})).toBeFalsy()
+  })
+})
