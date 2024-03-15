f47564c2f15b18176741db52e8120719df481ee0
jukerah <juka.mebaj@gmail.com>
2024-03-15 18:58:35 -0300

    feat: super request work with then, catch and finally

--- src/modules/super-request/index.ts
+++ src/modules/super-request/index.ts
@@ -2,55 +2,69 @@ import * as helper from './helpers'
 import * as type from '../types'
 
 export const superRequest = (app: any): type.SuperRequest => {
-  const createMethod = async (method: string, path: any, data: any, options?: Record<string, any>): Promise<type.SuperRequestMethod> => {
-    const httpRequest: any = helper.createHttpRequest({
-      method,
-      path,
-      headers: options?.headers ?? {},
-      data,
-      host: 'example.com',
-      port: 4000
+  const request = async (method: string, path: any, data: any, options?: Record<string, any>): Promise<any> => {
+    // eslint-disable-next-line no-async-promise-executor, @typescript-eslint/no-misused-promises
+    return await new Promise(async (resolve, reject) => {
+      try {
+        const httpRequest: any = helper.createHttpRequest({
+          method,
+          path,
+          headers: options?.headers ?? {},
+          data,
+          host: 'localhost',
+          port: Math.floor(10000 + Math.random() * 55535)
+        })
+
+        const httpResponse = helper.createHttpResponse(httpRequest)
+        const serverResponse = await app.serverMock(httpRequest, httpResponse)
+        serverResponse.data = helper.formatResponseData(serverResponse)
+        httpRequest.abort()
+
+        const response: type.SuperRequestMethod = {
+          statusCode: serverResponse.statusCode,
+          statusMessage: serverResponse.statusMessage,
+          headers: serverResponse.headers,
+          data: serverResponse.data
+        }
+
+        if (serverResponse.statusCode < 400) {
+          resolve(response)
+        } else {
+          const error = { response }
+          reject(error)
+        }
+      } catch (error) {
+        reject(error)
+      }
     })
-
-    const httpResponse = helper.createHttpResponse(httpRequest)
-    const serverResponse = await app.serverMock(httpRequest, httpResponse)
-    serverResponse.data = helper.formatResponseData(serverResponse)
-    httpRequest.abort()
-
-    return {
-      statusCode: serverResponse.statusCode,
-      statusMessage: serverResponse.statusMessage,
-      headers: serverResponse.headers,
-      data: serverResponse.data
-    }
   }
 
   const get = async (path: any, options?: Record<string, any>): Promise<type.SuperRequestMethod> => {
-    return await createMethod('GET', path, '', options)
+    return await request('GET', path, '', options)
   }
 
   const post = async (path: any, data: Record<string, any> | string, options?: Record<string, any>): Promise<type.SuperRequestMethod> => {
-    return await createMethod('POST', path, data, options)
+    return await request('POST', path, data, options)
   }
 
   const put = async (path: any, data: Record<string, any> | string, options?: Record<string, any>): Promise<type.SuperRequestMethod> => {
-    return await createMethod('PUT', path, data, options)
+    return await request('PUT', path, data, options)
   }
 
   const patch = async (path: any, data: Record<string, any> | string, options?: Record<string, any>): Promise<type.SuperRequestMethod> => {
-    return await createMethod('PATCH', path, data, options)
+    return await request('PATCH', path, data, options)
   }
 
   const remove = async (path: any, options?: Record<string, any>): Promise<type.SuperRequestMethod> => {
-    return await createMethod('DELETE', path, '', options)
+    return await request('DELETE', path, '', options)
   }
 
   const head = async (path: any, options?: Record<string, any>): Promise<type.SuperRequestMethod> => {
-    return await createMethod('HEAD', path, '', options)
+    return await request('HEAD', path, '', options)
   }
 
   const options = async (path: any, options?: Record<string, any>): Promise<type.SuperRequestMethod> => {
-    return await createMethod('OPTIONS', path, '', options)
+    return await request('OPTIONS', path, '', options)
   }
 
   return { get, post, put, patch, delete: remove, head, options }
