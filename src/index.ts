002ffb05b1ac175562f6a7d565b4a0ef747a142c
jukerah <juka.mebaj@gmail.com>
2024-03-27 16:55:26 -0300

    fix: export of modules

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-import * as vkrun from './public'
-export * from './public'
-export default vkrun
+import { App } from './modules/app'
+import { schema, setLocation } from './modules/schema'
+import { createLogger } from './modules/logger'
+import { Router } from './modules/router'
+import { parseData } from './modules/parse-data'
+import { cors } from './modules/cors'
+import { rateLimit } from './modules/rate-limit'
+import { Session } from './modules/session'
+import { validateRouteData } from './modules/validate-route-data'
+import { jwt } from './modules/jwt'
+import { superRequest } from './modules/super-request'
+export * from './modules/router/helpers/adapters'
+export * from './modules/utils'
+export * from './modules/types'
+
+export {
+  App,
+  cors,
+  schema,
+  setLocation,
+  createLogger,
+  Router,
+  parseData,
+  rateLimit,
+  jwt,
+  Session,
+  validateRouteData,
+  superRequest
+}
