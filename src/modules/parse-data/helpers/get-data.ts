29cf6dae5bd8b18e2113a6447b9f48e561e27832
Mario Botelho Alves Junior <mario.junior@adhosting.internal>
2024-10-11 00:29:38 -0300

    feat: simplifying getData and returning just the buffer

--- src/modules/parse-data/helpers/get-data.ts
+++ src/modules/parse-data/helpers/get-data.ts
@@ -1,57 +1,16 @@
-import * as util from '../../utils'
 import * as type from '../../types'
 
-export const getData = async (request: type.Request): Promise<string> => {
+export const getData = async (request: type.Request): Promise<Buffer> => {
   return await new Promise((resolve) => {
-    let body = ''
-    const files: any = []
-    const headerContentType = request.headers['content-type']
+    const rawData: any[] = []
 
-    if (util.isString(headerContentType) && headerContentType?.includes('multipart/form-data')) {
-      request.on('data', (chunk) => {
-        body += chunk.toString()
-      })
+    request.on('data', (chunk) => {
+      rawData.push(chunk)
+    })
 
-      request.on('end', () => {
-        const boundaryMatch = headerContentType.match(/boundary=(?:"([^"]+)"|([^;]+))/)
-        const boundary = boundaryMatch ? (boundaryMatch[1] || boundaryMatch[2]) : null
-
-        if (!boundary) {
-          resolve(body)
-        }
-
-        const parts = body.toString().split(`--${boundary}`)
-
-        for (const part of parts) {
-          if (part.includes('filename')) {
-            const file = { buffer: Buffer.alloc(0) }
-            const dataStartIndex = part.indexOf('\r\n\r\n') + 4
-            const fileData = part.substring(dataStartIndex)
-            file.buffer = Buffer.from(fileData)
-            files.push(file)
-          }
-        }
-
-        for (const file of files) {
-          const fileStartIndex = body.indexOf(file.buffer.toString())
-          if (fileStartIndex !== -1) {
-            body = body.slice(0, fileStartIndex) + body.slice(fileStartIndex + file.buffer.length)
-          }
-        }
-
-        body = body.trim()
-        resolve(body)
-      })
-    } else {
-      request.on('data', (chunk) => {
-        body += chunk.toString()
-      })
-
-      request.on('end', () => {
-        body = body.trim()
-
-        resolve(body)
-      })
-    }
+    request.on('end', () => {
+      const body = Buffer.concat(rawData)
+      resolve(body)
+    })
   })
 }
