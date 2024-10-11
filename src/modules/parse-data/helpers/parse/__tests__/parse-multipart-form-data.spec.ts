1dd4cef6d5a78abfe95b4ec1771aead738c4f1ef
Mario Botelho Alves Junior <mario.junior@adhosting.internal>
2024-10-11 00:43:56 -0300

    test: change tests to correctly validate the new return of parseMultipartFormData

--- src/modules/parse-data/helpers/parse/__tests__/parse-multipart-form-data.spec.ts
+++ src/modules/parse-data/helpers/parse/__tests__/parse-multipart-form-data.spec.ts
@@ -1,4 +1,5 @@
 import { parseMultipartFormData } from '../parse-multipart-form-data'
+import * as type from '../../../../types'
 
 describe('Parse Data - Parse Multipart Form Data', () => {
   it('Should parse body and return an object with the data if the body is not empty', () => {
@@ -36,11 +37,21 @@ describe('Parse Data - Parse Multipart Form Data', () => {
     const sut = parseMultipartFormData(request, false)
 
     expect(sut).toEqual({
-      string: 'any@mail.com',
-      integer: 123,
-      float: 1.56,
-      boolean: true,
-      date: new Date('2000-02-03T02:00:00.000Z')
+      body: {
+        string: 'any@mail.com',
+        integer: 123,
+        float: 1.56,
+        boolean: true,
+        date: new Date('2000-02-03T02:00:00.000Z')
+      },
+      files: [
+        {
+          buffer: '',
+          extension: 'txt',
+          filename: 'filename.txt',
+          mimetype: 'text/plain'
+        }
+      ]
     })
   })
 
@@ -59,18 +70,24 @@ describe('Parse Data - Parse Multipart Form Data', () => {
     const sut = parseMultipartFormData(request, true)
 
     expect(sut).toEqual({
-      sql: "'SELECT * FROM USER;'"
+      body: {
+        sql: "'SELECT * FROM USER;'"
+      },
+      files: []
     })
   })
 
-  it('Should parse body and return an object empty if the body is empty', () => {
+  it('Should parse body and return error if the body is empty', () => {
     const request: any = {
       headers: {},
       body: ''
     }
 
-    const sut = parseMultipartFormData(request, false)
+    const sut = (): {
+      body: object
+      files: type.File[]
+    } => parseMultipartFormData(request, false)
 
-    expect(sut).toEqual({})
+    expect(sut).toThrow('Boundary not found')
   })
 })
