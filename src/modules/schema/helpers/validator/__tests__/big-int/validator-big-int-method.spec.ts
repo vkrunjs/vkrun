d7f09730bf27b537d6d436be46ced70213fe5a33
jukerah <juka.mebaj@gmail.com>
2024-10-15 01:05:25 -0300

    test: add bigInt method to schema with sub methods min, max, positive and negative

---
+++ src/modules/schema/helpers/validator/__tests__/big-int/validator-big-int-method.spec.ts
@@ -0,0 +1,293 @@
+import { schema } from '../../../../index'
+import { AnyError } from '../../../../../errors'
+
+describe('Validator BigInt Method', () => {
+  it('Should be able to validate the bigInt method and return true if the value is of type bigint', () => {
+    const value = 1n
+
+    const sut = schema()
+      .bigInt()
+      .validate(value)
+
+    expect(sut).toBeTruthy()
+  })
+
+  it('Should be able to validate all sub methods of bigInt method and return true if the value is valid', () => {
+    // bigInt > min
+    expect(schema().bigInt().min(1n).validate(1n)).toBeTruthy()
+
+    // bigInt > min > max > ...
+    expect(schema().bigInt().min(1n).max(2n).validate(1n)).toBeTruthy()
+    expect(schema().bigInt().min(1n).max(2n).positive().validate(1n)).toBeTruthy()
+    expect(schema().bigInt().min(-2n).max(-1n).negative().validate(-1n)).toBeTruthy()
+
+    // bigInt > min > positive > ...
+    expect(schema().bigInt().min(1n).positive().validate(1n)).toBeTruthy()
+    expect(schema().bigInt().min(1n).positive().max(2n).validate(1n)).toBeTruthy()
+
+    // bigInt > min > negative > ...
+    expect(schema().bigInt().min(-2n).negative().validate(-1n)).toBeTruthy()
+    expect(schema().bigInt().min(-2n).negative().max(-1n).validate(-1n)).toBeTruthy()
+
+    // bigInt > max
+    expect(schema().bigInt().max(2n).validate(1n)).toBeTruthy()
+
+    // bigInt > max > max > ...
+    expect(schema().bigInt().max(2n).min(1n).validate(1n)).toBeTruthy()
+    expect(schema().bigInt().max(2n).min(1n).positive().validate(1n)).toBeTruthy()
+    expect(schema().bigInt().max(-1n).min(-2n).negative().validate(-1n)).toBeTruthy()
+
+    // bigInt > max > positive > ...
+    expect(schema().bigInt().max(2n).positive().validate(1n)).toBeTruthy()
+    expect(schema().bigInt().max(2n).positive().min(1n).validate(1n)).toBeTruthy()
+
+    // bigInt > max > negative > ...
+    expect(schema().bigInt().max(-1n).negative().validate(-1n)).toBeTruthy()
+    expect(schema().bigInt().max(-1n).negative().min(-2n).validate(-1n)).toBeTruthy()
+
+    // bigInt > positive
+    expect(schema().bigInt().positive().validate(1n)).toBeTruthy()
+
+    // bigInt > positive > min > ...
+    expect(schema().bigInt().positive().min(1n).validate(1n)).toBeTruthy()
+    expect(schema().bigInt().positive().min(1n).max(2n).validate(1n)).toBeTruthy()
+
+    // bigInt > positive > max > ...
+    expect(schema().bigInt().positive().max(2n).validate(1n)).toBeTruthy()
+    expect(schema().bigInt().positive().max(2n).min(1n).validate(1n)).toBeTruthy()
+
+    // bigInt > negative
+    expect(schema().bigInt().negative().validate(-1n)).toBeTruthy()
+
+    // bigInt > negative > min > ...
+    expect(schema().bigInt().negative().min(-2n).validate(-1n)).toBeTruthy()
+    expect(schema().bigInt().negative().min(-2n).max(-1n).validate(-1n)).toBeTruthy()
+
+    // bigInt > negative > max > ...
+    expect(schema().bigInt().negative().max(-1n).validate(-1n)).toBeTruthy()
+    expect(schema().bigInt().negative().max(-1n).min(-2n).validate(-1n)).toBeTruthy()
+  })
+
+  it('Should be able to validate the bigInt method and return false if list is invalid', () => {
+    const invalidList = [
+      'invalid-uuid-57b73598-8764-4ad0-a76a-679bb6640eb1',
+      false,
+      new Date(),
+      null,
+      [],
+      {},
+      undefined
+    ]
+
+    const sut = schema().bigInt()
+
+    expect(invalidList.every((value) => sut.validate(value))).toBeFalsy()
+  })
+
+  it('Should be able to validate the bigInt method when value is promise and return true if the value is of type bigint', async () => {
+    const value = async (): Promise<bigint> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(1n)
+        }, 100)
+      })
+    }
+
+    const sut = await schema()
+      .bigInt()
+      .validateAsync(value())
+
+    expect(sut).toBeTruthy()
+  })
+
+  it('Should be able to validate the bigInt method when value is promise and return false if the value is not of type bigint', async () => {
+    const value = async (): Promise<boolean> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(false)
+        }, 100)
+      })
+    }
+
+    const sut = await schema()
+      .bigInt()
+      .validateAsync(value())
+
+    expect(sut).toBeFalsy()
+  })
+
+  it('Should be able to validate the bigInt method and passedAll to equal true if the value is of type bigint', () => {
+    const value = 1n
+
+    const sut = schema()
+      .bigInt()
+      .test(value, 'value_name')
+
+    expect(sut.passedAll).toBeTruthy()
+    expect(sut.passed).toEqual(2)
+    expect(sut.failed).toEqual(0)
+    expect(sut.totalTests).toEqual(2)
+    expect(sut.successes).toEqual([
+      {
+        method: 'required',
+        name: 'value_name',
+        expect: 'value other than undefined',
+        received: 1n
+      },
+      {
+        method: 'bigInt',
+        expect: 'bigint type',
+        name: 'value_name',
+        received: 1n
+      }
+    ])
+    expect(sut.errors).toEqual([])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the bigInt method and passedAll to equal false if the value is not of type bigint', () => {
+    const value = false
+
+    const sut = schema()
+      .bigInt()
+      .test(value, 'value_name')
+
+    expect(sut.passedAll).toBeFalsy()
+    expect(sut.passed).toEqual(1)
+    expect(sut.failed).toEqual(1)
+    expect(sut.totalTests).toEqual(2)
+    expect(sut.successes).toEqual([{
+      method: 'required',
+      name: 'value_name',
+      expect: 'value other than undefined',
+      received: false
+    }])
+    expect(sut.errors).toEqual([{
+      method: 'bigInt',
+      type: 'invalid value',
+      name: 'value_name',
+      expect: 'bigint type',
+      received: false,
+      message: 'value_name must be a bigint type!'
+    }])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the bigInt method and passAll method as equal to true when it is not required, undefined value and not of type bigint', () => {
+    const value = undefined
+
+    const sut = schema()
+      .bigInt()
+      .notRequired()
+      .test(value, 'value_name')
+
+    expect(sut.passedAll).toBeTruthy()
+    expect(sut.passed).toEqual(1)
+    expect(sut.failed).toEqual(0)
+    expect(sut.totalTests).toEqual(1)
+    expect(sut.successes).toEqual([{
+      method: 'notRequired',
+      name: 'value_name',
+      expect: 'value is not required and of any type',
+      received: 'undefined'
+    }])
+    expect(sut.errors).toEqual([])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the bigInt method and passedAll to equal true if the value is promise of type bigint', async () => {
+    const value = async (): Promise<bigint> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(1n)
+        }, 100)
+      })
+    }
+
+    const sut = await schema()
+      .bigInt()
+      .testAsync(value(), 'value_name')
+
+    expect(sut.passedAll).toBeTruthy()
+    expect(sut.passed).toEqual(2)
+    expect(sut.failed).toEqual(0)
+    expect(sut.totalTests).toEqual(2)
+    expect(sut.successes).toEqual([
+      {
+        method: 'required',
+        name: 'value_name',
+        expect: 'value other than undefined',
+        received: 1n
+      },
+      {
+        method: 'bigInt',
+        expect: 'bigint type',
+        name: 'value_name',
+        received: 1n
+      }
+    ])
+    expect(sut.errors).toEqual([])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the bigInt method and passedAll to equal false if the value is a promise and is not of type bigint', async () => {
+    const value = async (): Promise<boolean> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(false)
+        }, 100)
+      })
+    }
+
+    const sut = await schema()
+      .bigInt()
+      .testAsync(value(), 'value_name')
+
+    expect(sut.passedAll).toBeFalsy()
+    expect(sut.passed).toEqual(1)
+    expect(sut.failed).toEqual(1)
+    expect(sut.totalTests).toEqual(2)
+    expect(sut.successes).toEqual([{
+      method: 'required',
+      name: 'value_name',
+      expect: 'value other than undefined',
+      received: false
+    }])
+    expect(sut.errors).toEqual([{
+      method: 'bigInt',
+      type: 'invalid value',
+      name: 'value_name',
+      expect: 'bigint type',
+      received: false,
+      message: 'value_name must be a bigint type!'
+    }])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the bigInt method and throw AnyError if the value is not of type bigint', () => {
+    const value = undefined
+
+    const sut = (): void => schema()
+      .bigInt()
+      .throw(value, 'value_name', AnyError)
+
+    expect(sut).toThrow(AnyError)
+    expect(sut).toThrow(new AnyError('value_name is required!'))
+  })
+
+  it('Should be able to validate the bigInt method and throw Error if the value is a promise and is not of type bigint', async () => {
+    const value = async (): Promise<null> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(null)
+        }, 100)
+      })
+    }
+
+    const sut = async (): Promise<void> => await schema()
+      .bigInt()
+      .throwAsync(value(), 'value_name')
+
+    await expect(sut).rejects.toThrow('value_name must be a bigint type!')
+  })
+})
---
+++ src/modules/schema/helpers/validator/__tests__/big-int/validator-min-method.spec.ts
@@ -0,0 +1,314 @@
+import { schema } from '../../../../index'
+import { AnyError } from '../../../../../errors'
+
+describe('Validator Min BigInt Method', () => {
+  it('Should be able to validate the min method and return true if the value is greater than or equal to the reference', () => {
+    expect(
+      schema().bigInt().min(5n).validate(5n)
+    ).toBeTruthy()
+    expect(
+      schema().bigInt().min(1n).max(5n).validate(5n)
+    ).toBeTruthy()
+    expect(
+      schema().bigInt().negative().max(-1n).validate(-5n)
+    ).toBeTruthy()
+    expect(
+      schema().bigInt().negative().min(-5n).max(-1n).validate(-5n)
+    ).toBeTruthy()
+    expect(
+      schema().bigInt().positive().max(5n).validate(5n)
+    ).toBeTruthy()
+    expect(
+      schema().bigInt().positive().min(1n).max(5n).validate(5n)
+    ).toBeTruthy()
+  })
+
+  it('Should be able to validate the min method and return false if list is invalid', () => {
+    const invalidList = [
+      5.5,
+      6n,
+      '1',
+      false,
+      new Date(),
+      null,
+      [],
+      {},
+      undefined
+    ]
+
+    const sut = schema()
+      .bigInt()
+      .min(7n)
+
+    expect(invalidList.every((value) => sut.validate(value))).toBeFalsy()
+  })
+
+  it('Should be able to validate the min method when value is promise and return true if the value is greater than or equal to the reference', async () => {
+    const value = async (): Promise<bigint> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(1n)
+        }, 100)
+      })
+    }
+
+    const sut = await schema()
+      .bigInt()
+      .min(1n)
+      .validateAsync(value())
+
+    expect(sut).toBeTruthy()
+  })
+
+  it('Should be able to validate the min method when value is promise and return false if the value is greater than to the reference', async () => {
+    const value = async (): Promise<bigint> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(1n)
+        }, 100)
+      })
+    }
+
+    const sut = await schema()
+      .bigInt()
+      .min(5n)
+      .validateAsync(value())
+
+    expect(sut).toBeFalsy()
+  })
+
+  it('Should be able to validate the min method and passedAll to equal true if the value is value greater than or equal to the reference', () => {
+    const value = 1n
+
+    const sut = schema()
+      .bigInt()
+      .min(1n)
+      .test(value, 'value_name')
+
+    expect(sut.passedAll).toBeTruthy()
+    expect(sut.passed).toEqual(3)
+    expect(sut.failed).toEqual(0)
+    expect(sut.totalTests).toEqual(3)
+    expect(sut.successes).toEqual([
+      {
+        method: 'required',
+        name: 'value_name',
+        expect: 'value other than undefined',
+        received: 1n
+      },
+      {
+        method: 'bigInt',
+        name: 'value_name',
+        expect: 'bigint type',
+        received: 1n
+      },
+      {
+        method: 'min',
+        name: 'value_name',
+        expect: 'value greater than or equal to the reference',
+        received: 1n
+      }
+    ])
+    expect(sut.errors).toEqual([])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the min method and passedAll to equal false if the value is greater than to the reference', () => {
+    const value = 4n
+
+    const sut = schema()
+      .bigInt()
+      .min(5n)
+      .test(value, 'value_name')
+
+    expect(sut.passedAll).toBeFalsy()
+    expect(sut.passed).toEqual(2)
+    expect(sut.failed).toEqual(1)
+    expect(sut.totalTests).toEqual(3)
+    expect(sut.successes).toEqual([
+      {
+        method: 'required',
+        name: 'value_name',
+        expect: 'value other than undefined',
+        received: 4n
+      },
+      {
+        method: 'bigInt',
+        name: 'value_name',
+        expect: 'bigint type',
+        received: 4n
+      }
+    ])
+    expect(sut.errors).toEqual([{
+      method: 'min',
+      type: 'invalid value',
+      name: 'value_name',
+      expect: 'value greater than or equal to the reference',
+      received: 4n,
+      message: 'value_name must be greater than or equal to 5n!'
+    }])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the min and passAll method as equal to true when it is not required', () => {
+    const value = undefined
+
+    const sut = schema()
+      .bigInt()
+      .min(5n)
+      .notRequired()
+      .test(value, 'value_name')
+
+    expect(sut.passedAll).toBeTruthy()
+    expect(sut.passed).toEqual(1)
+    expect(sut.failed).toEqual(0)
+    expect(sut.totalTests).toEqual(1)
+    expect(sut.successes).toEqual([{
+      method: 'notRequired',
+      name: 'value_name',
+      expect: 'value is not required and of any type',
+      received: 'undefined'
+    }])
+    expect(sut.errors).toEqual([])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the min and passAll method as equal to true when it is nullable', () => {
+    const value = null
+
+    const sut = schema()
+      .bigInt()
+      .min(5n)
+      .nullable()
+      .test(value, 'value_name')
+
+    expect(sut.passedAll).toBeTruthy()
+    expect(sut.passed).toEqual(1)
+    expect(sut.failed).toEqual(0)
+    expect(sut.totalTests).toEqual(1)
+    expect(sut.successes).toEqual([{
+      method: 'nullable',
+      name: 'value_name',
+      expect: 'the value can be null, but other than undefined',
+      received: null
+    }])
+    expect(sut.errors).toEqual([])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the min method and passedAll to equal true if the value is promise with a smaller bigint than the reference', async () => {
+    const value = async (): Promise<bigint> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(1n)
+        }, 100)
+      })
+    }
+
+    const sut = await schema()
+      .bigInt()
+      .min(1n)
+      .testAsync(value(), 'value_name')
+
+    expect(sut.passedAll).toBeTruthy()
+    expect(sut.passed).toEqual(3)
+    expect(sut.failed).toEqual(0)
+    expect(sut.totalTests).toEqual(3)
+    expect(sut.successes).toEqual([
+      {
+        method: 'required',
+        name: 'value_name',
+        expect: 'value other than undefined',
+        received: 1n
+      },
+      {
+        method: 'bigInt',
+        expect: 'bigint type',
+        name: 'value_name',
+        received: 1n
+      },
+      {
+        method: 'min',
+        expect: 'value greater than or equal to the reference',
+        name: 'value_name',
+        received: 1n
+      }
+    ])
+    expect(sut.errors).toEqual([])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the min method and passedAll to equal false if the value is a promise and is not bigint', async () => {
+    const value = async (): Promise<boolean> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(false)
+        }, 100)
+      })
+    }
+
+    const sut = await schema()
+      .bigInt()
+      .min(5n)
+      .testAsync(value(), 'value_name')
+
+    expect(sut.passedAll).toBeFalsy()
+    expect(sut.passed).toEqual(1)
+    expect(sut.failed).toEqual(2)
+    expect(sut.totalTests).toEqual(3)
+    expect(sut.successes).toEqual([{
+      method: 'required',
+      name: 'value_name',
+      expect: 'value other than undefined',
+      received: false
+    }])
+    expect(sut.errors).toEqual([
+      {
+        method: 'bigInt',
+        type: 'invalid value',
+        name: 'value_name',
+        expect: 'bigint type',
+        received: false,
+        message: 'value_name must be a bigint type!'
+      },
+      {
+        method: 'min',
+        type: 'invalid value',
+        name: 'value_name',
+        expect: 'value greater than or equal to the reference',
+        received: false,
+        message: 'value_name must be greater than or equal to 5n!'
+      }
+    ])
+    expect(typeof sut.time === 'string').toBeTruthy()
+  })
+
+  it('Should be able to validate the min method and throw AnyError if the value is not bigint', () => {
+    const value = undefined
+
+    const sut = (): void => schema()
+      .bigInt()
+      .min(5n)
+      .throw(value, 'value_name', AnyError)
+
+    expect(sut).toThrow(AnyError)
+    expect(sut).toThrow(new AnyError('value_name is required!'))
+  })
+
+  it('Should be able to validate the min method and throw Error if the value is a promise and is not bigint', async () => {
+    const value = async (): Promise<string> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('1')
+        }, 100)
+      })
+    }
+
+    const sut = async (): Promise<void> => await schema()
+      .bigInt()
+      .min(5n)
+      .throwAsync(value(), 'value_name')
+
+    await expect(sut).rejects.toThrow('value_name must be a bigint type!')
+  })
+})
