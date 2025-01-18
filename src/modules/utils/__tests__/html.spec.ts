0ebcf91970b1d47063278095c332f2050887fe8d
jukerah <juka.mebaj@gmail.com>
2025-01-18 17:05:34 -0300

    feat: add html utility to render html template string with html color palette

---
+++ src/modules/utils/__tests__/html.spec.ts
@@ -0,0 +1,10 @@
+import { html } from '../html'
+
+describe('html', () => {
+  it('Should return string html', () => {
+    const stringHtml = html`<html><head><title>Test</title></head><body><h1>Hello World!</h1></body></html>`
+    expect(stringHtml).toEqual(
+      '<html><head><title>Test</title></head><body><h1>Hello World!</h1></body></html>'
+    )
+  })
+})
---
+++ src/modules/utils/html.ts
@@ -0,0 +1,5 @@
+export const html = (strings: TemplateStringsArray, ...values: unknown[]): string =>
+  strings.reduce((acc, str, i) => {
+    const value = values[i]
+    return acc + str + (typeof value === 'string' ? value : String(value ?? ''))
+  }, '')
